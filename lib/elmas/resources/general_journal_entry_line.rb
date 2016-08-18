fe5d067a8ae01298daf7a94caff25a2b121bdae7
Jason Lombardozzi <jason@stayntouch.com>
2016-08-18 11:36:09 -0400

    Add attributes

--- lib/elmas/resources/general_journal_entry_line.rb
+++ lib/elmas/resources/general_journal_entry_line.rb
@@ -5,5 +5,13 @@ module Elmas
     def base_path
       "generaljournalentry/GeneralJournalEntryLines"
     end
+
+    def other_attributes
+      [
+        :account, :amount_VATFC, :asset, :cost_center, :cost_unit, :date,
+        :description, :notes, :document, :exchange_rate, :our_ref,
+        :project, :quantity, :VAT_code, :VAT_percentage, :VAT_type
+      ]
+    end
   end
 end
